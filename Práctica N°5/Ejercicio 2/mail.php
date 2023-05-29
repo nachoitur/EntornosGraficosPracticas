e3824e2d0dc86ded052d02be6e8f2773e33c9bab
nachoitur <86988960+nachoitur@users.noreply.github.com>
2023-05-29 20:34:18 -0300

    Finalización ejercicio 2

---
+++ Práctica N°5/Ejercicio 2/mail.php
@@ -0,0 +1,23 @@
+<?php
+
+$fecha=date("d-m-Y");
+$hora= date("H :i:s");
+$destino="ignacioiturburu07@gmail.com";
+$asunto="Comentario de ejercicio 2";
+
+$nombre = $_POST['nombre'];
+$desde= $_POST['email'];
+$mensaje= ""$_POST['mensaje']."\n
+Enviado: $fecha a las $hora\n
+\n";
+
+$header = "From: $nombre <$desde>\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-Type: text/html; charset=utf-8\r\n";
+
+if (mail($destino,$asunto,$mensaje,$header)){
+  echo "Correo enviado correctamente.";
+} else {
+  echo "Error al enviar el correo.";
+};
+?>
