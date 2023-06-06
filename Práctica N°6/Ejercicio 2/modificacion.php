02ef435aac5d0f819a8418d1aeee1775cd1b1472
Nacho Iturburu <ignacioiturburu07@gmail.com>
2023-06-06 19:27:32 -0300

    Finalización ejercicio 2, falta testear

---
+++ Práctica N°6/Ejercicio 2/modificacion.php
@@ -0,0 +1,21 @@
+<head>
+    <title>Modificacion</title>
+</head>
+<body>
+    <?php
+    include ("conexion.php");
+
+    $ciudad = $_POST['ciudad'];
+    $pais = $_POST['pais'];
+    $habitantes = $_POST['habitantes'];
+    $superficie = $_POST['superficie'];
+    $tieneMetro = $_POST['tieneMetro'];
+
+    $query = "UPDATE ciudades set ciudad='$ciudad', pais='$pais', habitantes='$habitantes', superficie='$superficie', tieneMetro=$tieneMetro where ciudad='$ciudad'";
+    mysqli_query($link,$query) or die (mysqli_error($link));
+    echo("La ciudad fue modificada<br>");
+    echo("<a href= 'menu.html'>Volver al menú del ABM</a>");
+    mysqli_close($link);
+?>
+</body>
+</html>
